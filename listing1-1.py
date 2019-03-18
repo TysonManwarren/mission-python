7ea5a0096f53d26f12ef71ebebd9988a230e306c
Tyson Manwarren <tmanwarren@gmail.com>
2019-03-17 21:52:08 -0400

    Progress - Chapter 4

--- listing1-1.py
+++ listing1-1.py
@@ -22,8 +22,10 @@ def game_loop():
     elif keyboard.left:
         player_x -= 5
     elif keyboard.up:
-        player_y -= 5
+        player_y -= 10
     elif keyboard.down:
-        player_y += 5
+        player_y += 10
 
 clock.schedule_interval(game_loop, 0.03)
+
+
